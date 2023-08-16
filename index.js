6cd472dbdf14c73a5f8445405523641a94f28f56
Rahmatullah Arrizal <rahmatullaharrizal@gmail.com>
2023-08-17 00:29:52 +0700

    ci cd

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/hello', (req, res) => {
   res.send('World!')
+})
+
+app.post('/sample', (req, res) => {
+    res.send(req.body)
 })
 
 app.listen(port, () => {
